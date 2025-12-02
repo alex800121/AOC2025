eeaaa0ed9121bf0472e7f6e6998f15c53ca0c19b
alex800121 <alex800121@hotmail.com>
2025-12-02 08:57:22 +0800

    day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,4 +1,32 @@
 module Day1 where
 
+import Data.List
+import Paths_AOC2025 (getDataDir)
+import Text.Read (readMaybe)
+
 day1 :: IO ()
-day1 = return ()
+day1 = do
+  input <- lines <$> (readFile . (++ "/input/input1.txt") =<< getDataDir)
+  -- input <- lines <$> (readFile . (++ "/input/test1.txt") =<< getDataDir)
+  putStrLn
+    . ("day1a: " ++)
+    . show
+    . length
+    . filter (== 0)
+    $ scanl' (\acc x -> (acc + (case x of 'R' : n -> read n; 'L' : n -> -(read n))) `mod` 100) 50 input
+  putStrLn
+    . ("day1b: " ++)
+    . show
+    . fst
+    $ foldl' f (0, 50) input
+
+f (acc, n) x
+  | 'R' : x <- x
+  , Just x <- readMaybe @Int x
+  , (d, m) <- (x + n) `divMod` 100 =
+      (acc + d, m)
+  | 'L' : x <- x
+  , Just x <- readMaybe @Int x
+  , (d, m) <- (((100 - n) `mod` 100) + x) `divMod` 100 =
+      (acc + d, (100 - m) `mod` 100)
+
