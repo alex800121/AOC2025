4a899340cee4a94d7df284a37417abe08daea43d
alex800121 <alex800121@hotmail.com>
2025-12-03 18:05:39 +0800

    day3

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,15 +1,26 @@
 module Day3 where
 
+import Data.List
 import Paths_AOC2025 (getDataDir)
 
 day3 :: IO ()
 day3 = do
-  -- input <- (readFile . (++ "/input/input3.txt") =<< getDataDir)
+  input <- lines <$> (readFile . (++ "/input/input3.txt") =<< getDataDir)
   putStrLn
     . ("day3a: " ++)
     . show
-    $ ()
+    . sum
+    $ map (read @Int . f 2) input
   putStrLn
     . ("day3b: " ++)
     . show
-    $ ()
+    . sum
+    $ map (read @Int . f 12) input
+
+f n input = foldr g xs ys
+  where
+    (ys, xs) = splitAt (length input - n) input
+    g _ [] = []
+    g y (z : zs)
+      | z > y = z : zs
+      | otherwise = y : g z zs
