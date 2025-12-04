932252f5be015d233f5da87408b7c133bfc571ae
alex800121 <alex800121@hotmail.com>
2025-12-04 17:43:09 +0800

    day4

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,15 +1,37 @@
 module Day4 where
 
+import Data.Array.IArray qualified as IA
+import Data.Array.Unboxed (UArray)
+import MyLib (drawArray, (+&))
 import Paths_AOC2025 (getDataDir)
 
+type Index = (Int, Int)
+
+adjacent = [(x, y) | x <- [-1 .. 1], y <- [-1 .. 1], (x, y) /= (0, 0)]
+day4a a =
+    [ (i, '.')
+    | (i, '@') <- IA.assocs a
+    , let is = map (i +&) adjacent
+    , length (filter ((== Just '@') . (a IA.!?)) is) < 4
+    ]
+
+day4b = go 0
+  where
+    go acc x 
+      | null removed = acc
+      | otherwise = go (acc + length removed) x'
+      where
+        removed = day4a x
+        x' = x IA.// removed
 day4 :: IO ()
 day4 = do
-  -- input <- (readFile . (++ "/input/input4.txt") =<< getDataDir)
+  input <- drawArray @UArray . lines <$> (readFile . (++ "/input/input4.txt") =<< getDataDir)
   putStrLn
     . ("day4a: " ++)
     . show
-    $ ()
+    . length
+    $ day4a input
   putStrLn
     . ("day4b: " ++)
     . show
-    $ ()
+    $ day4b input
