5809acc8c04ec4b2acd9c714f3069926a471c430
alex800121 <alex800121@hotmail.com>
2025-12-04 17:43:09 +0800

    day4

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,15 +1,43 @@
 module Day2 where
 
+import Data.Function (fix)
+import Data.List.Split (splitOn)
 import Paths_AOC2025 (getDataDir)
+import Prelude hiding (log)
+import Data.Containers.ListUtils (nubOrd)
 
 day2 :: IO ()
 day2 = do
-  -- input <- (readFile . (++ "/input/input2.txt") =<< getDataDir)
+  input <-
+    map ((\[x, y] -> (read @Int x, read @Int y)) . splitOn "-")
+      . splitOn ","
+      <$> (readFile . (++ "/input/input2.txt") =<< getDataDir)
+  let m = log 10 $ maximum $ map snd input
   putStrLn
     . ("day2a: " ++)
     . show
-    $ ()
+    . sum
+    $ concatMap (validA [2]) input
   putStrLn
     . ("day2b: " ++)
     . show
-    $ ()
+    . sum
+    $ concatMap (validA [2..m]) input
+
+validA xs (x, y) =
+  nubOrd
+    [ zs
+    | n <- xs
+    , n > 1
+    , xy <- [x10 + 1, y10 + 1]
+    , let (d, m) = xy `divMod` n
+    , m == 0 && d > 0
+    , z <- [10 ^ (d - 1) .. (10 ^ d) - 1]
+    , let zs = z * sum (take n (iterate (* (10 ^ d)) 1))
+    , zs >= x && zs <= y
+    ]
+ where
+  x10 = log 10 x
+  y10 = log 10 y
+
+log = fix (\f c y -> let y' = y `div` c in if y' > 0 then 1 + f c y' else 0)
